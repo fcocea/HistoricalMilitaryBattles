410eb9d5f74da02c59920db474908745e4dcec7e
Francisco Cea <fcoceak@gmail.com>
2024-06-12 13:35:14 -0400

    animation finished

--- map.py
+++ map.py
@@ -1,18 +1,78 @@
 import plotly.express as px
 from dash_manager import app
-from dash import Output, Input
+from dash import dcc, Output, Input, State
+import pandas as pd
+
+
+df = pd.read_csv('https://gist.githubusercontent.com/shinokada/f01139d3a024de375ede23cec5d52360/raw/424ac0055ed71a04e6f45badfaef73df96ad0aad/CrimeStatebyState_1960-2014.csv')
+df = df[(df['State'] != 'District of Columbia')]
+
+_years = df['Year'].unique()
+_years.sort()
+_years = _years.tolist()
+
 
 @app.callback(
-    [Output("historical_map", "figure")],
-    [Input("range-slider-callback", "value")]
+    [Output("historical_map", "figure"), Output("map-divider", "label")],
+    [Input("animation_state", "data")]
+)
+def get_map(state):
+    current_year = state['current_year']
+    df_year = df[df['Year'] ==
+                 current_year] if current_year is not None else df[df['Year'] == _years[0]]
+    map_fig = px.choropleth(df_year,
+                            locations='State_code',
+                            color="Murder_per100000",
+                            color_continuous_scale="Inferno",
+                            locationmode='USA-states',
+                            scope="usa",
+                            range_color=(0, 20),
+                            title='Crime by State',
+                            height=600,
+                            )
+    return [map_fig, f"Año {current_year}"]
+
+
+app.clientside_callback(
+    """
+    function(n_clicks, current_state) {
+        var graphDiv = document.querySelector('#historical_map').children[1];
+        var new_state = current_state.state === 'paused' ? 'playing' : 'paused';
+
+        if (new_state === 'playing') {
+            return ["radix-icons:pause", {
+                'state': new_state,
+                'current_year': current_state['current_year']
+            }];
+        } else {
+
+            return ["radix-icons:play", {
+                'state': new_state,
+                'current_year': current_state['current_year']
+            }];
+        }
+    }
+    """,
+    [Output('button_icon', 'icon'),
+     Output('animation_state', 'data')],
+    [Input('button_toggle', 'n_clicks'), State('animation_state', 'data')],
+    prevent_initial_call=True
 )
 
 
-def get_map(selected_years):
-    historical_map = px.choropleth()
-    historical_map.update_geos(projection_type="natural earth")
-    historical_map.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    historical_map.update_layout(dragmode=False)
-
-
-    return [historical_map]
+@app.callback(
+    Output('animation_state', 'data', allow_duplicate=True),
+    [Input('interval', 'n_intervals')],
+    [State('animation_state', 'data')],
+    prevent_initial_call=True
+)
+def update_current_year(n_intervals, animation_state):
+    if animation_state['state'] == 'playing':
+        current_year = animation_state['current_year']
+        if current_year is None or current_year == _years[-1]:
+            current_year = _years[0]
+        else:
+            current_year = _years[_years.index(current_year) + 1]
+        return {'state': 'playing', 'current_year': current_year}
+    else:
+        return animation_state
--- ui/layout.py
+++ ui/layout.py
@@ -3,54 +3,59 @@ import dash_mantine_components as dmc
 from dash_manager import app
 from conflicts import countries
 from map import get_map
+from dash_iconify import DashIconify
 
-__years = [{"value": year, "label": year} for year in range(1600, 1974, 50)] + [{"value": 1973, "label": 1973}]
+__years = [{"value": year, "label": year}
+           for year in range(1600, 1974, 50)] + [{"value": 1973, "label": 1973}]
+
 
 def get_layout():
-    return dmc.MantineProvider(html.Div([
-    html.Div([
-        dmc.Title("Batallas militares históricas", order=1),
-        dmc.Text("Condiciones y resultados de más de 600 batallas libradas entre 1600 y 1973 d.C.", size="sm"),
-    ], className="title-container"),
-
-    html.Div([
-        dcc.Graph(id='historical_map',
-                  config={'displayModeBar': False}),
-    ], className="map-container"),
-
-    html.Div(
-    [
-       dmc.RangeSlider(
-            id="range-slider-callback",
-            marks=__years,
-            max=1973,
-            min=1600,
-            mb=35,
-            minRange=25,
-            labelAlwaysOn=True,
-            value=[1600,1973]
+    return dmc.MantineProvider(dmc.Flex([
+        html.Div([
+            dmc.Title("Batallas militares históricas", order=1),
+            dmc.Text(
+                "Condiciones y resultados de más de 600 batallas libradas entre 1600 y 1973 d.C.", size="sm"),
+        ], className="title-container"),
+        dmc.Divider(variant="solid", label="Año 1960", id="map-divider"),
+        dmc.Flex([
+            dcc.Graph(id='historical_map', animate=True, animation_options={
+                      'frame': {'redraw': True}}, config={'displayModeBar': False}, ),
+            dmc.Flex([
+                dmc.Button(
+                    DashIconify(width=24,
+                                icon="radix-icons:play", id="button_icon"),
+                    size="xs",
+                    id="button_toggle"
+                ),
+                dcc.Store(id='animation_state', data={
+                          'state': 'paused', 'current_year': 1960}),
+                dcc.Interval(id='interval', interval=1000, n_intervals=0)
+            ], justify="center", gap="xs"),
+        ], direction="column", gap="lg"),
+        dmc.Divider(variant="solid"),
+        dmc.Flex(
+            [
+                dmc.RangeSlider(
+                    id="range-slider-callback",
+                    marks=__years,
+                    max=1973,
+                    min=1600,
+                    mb=35,
+                    minRange=25,
+                    labelAlwaysOn=True,
+                    value=[1600, 1973]
+                ),
+                dmc.Select(
+                    id='country-dropdown',
+                    data=countries,
+                    searchable=True,
+                    value='USA',
+                    allowDeselect=False,
+                ),
+            ], direction="column", gap="sm",
         ),
-        dmc.Space(h=10),
-        dmc.Text(id="range-slider-output"), 
-    ]
-    ),
-        dmc.Select(
-        id='country-dropdown',
-        data=countries,
-        searchable=True,
-        value='USA',
-        allowDeselect=False,
-    ),
-    dmc.Flex([
-        dcc.Graph(id='graph'),
-        dcc.Graph(id='pie-chart')
-    ], direction={"base": "column", "sm": "row"}, w={"base": "100%", "sm": "100%"}, gap="lg")
-]))
-
-
-@app.callback(
-    Output("range-slider-output", "children"), Input("range-slider-callback", "value")
-)
-def update_value(value):
-    return f"You have selected: [{value[0]}, {value[1]}]"
-
+        dmc.Flex([
+            dcc.Graph(id='graph'),
+            dcc.Graph(id='pie-chart')
+        ], direction={"base": "column", "sm": "row"}, w="100%", gap="lg", justify="space-between"),
+    ], gap="lg", direction="column"))
