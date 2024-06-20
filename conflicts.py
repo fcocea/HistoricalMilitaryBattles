b0688fa939cf4e80a0898b7254e67adca532f0f8
Francisco Cea <fcoceak@gmail.com>
2024-06-20 01:04:19 -0400

    fix fstring

--- conflicts.py
+++ conflicts.py
@@ -47,18 +47,19 @@ for index, row in df_results.iterrows():
     df_results.loc[df_results['country'] == country, 'lost'] = lost
     df_results.loc[df_results['country'] == country, 'draws'] = draws
 
+
 @app.callback(
     [Output('graph', 'figure'),
-     Output('pie-chart', 'figure'), 
-     Output('graph-title', 'children'), 
+     Output('pie-chart', 'figure'),
+     Output('graph-title', 'children'),
      Output('pie-chart-title', 'children'),
-     Output('graph-subtitle','children'),
-     Output('pie-chart-subtitle','children')],
+     Output('graph-subtitle', 'children'),
+     Output('pie-chart-subtitle', 'children')],
     [Input('country-dropdown', 'value'),
      Input('contrincante-dropdown', 'value')]
 )
 def update_graph(selected_country, selected_contrincante=None):
-    colors = ['#3399FF','#DC143C','#808080']
+    colors = ['#3399FF', '#DC143C', '#808080']
     if selected_contrincante is None:
         # Gráfico de barras para ataques y defensas
         df_attacks = conflicts[conflicts['defender'] == selected_country]
@@ -70,7 +71,7 @@ def update_graph(selected_country, selected_contrincante=None):
 
         # Datos de defensa y ataque para todos los países
         defense_data = {country: -df_attacks.loc[df_attacks['attacker']
-                                                == country, 'count'].sum() for country in all_countries}
+                                                 == country, 'count'].sum() for country in all_countries}
         attack_data = {country: df_defenses.loc[df_defenses['defender'] == country, 'count'].sum(
         ) for country in all_countries}
 
@@ -118,9 +119,9 @@ def update_graph(selected_country, selected_contrincante=None):
         df_selected = df_results[df_results['country'] == selected_country]
         pie_fig = go.Figure(data=[go.Pie(
             labels=[
-                f'Ganadas: {df_selected['won'].values[0]}', 
-                f'Perdidas: {df_selected['lost'].values[0]}', 
-                f'Empatadas: {df_selected['draws'].values[0]}'
+                f"Ganadas: {df_selected['won'].values[0]}",
+                f"Perdidas: {df_selected['lost'].values[0]}",
+                f"Empatadas: {df_selected['draws'].values[0]}"
             ],
             values=[df_selected['won'].values[0],
                     df_selected['lost'].values[0], df_selected['draws'].values[0]],
@@ -146,17 +147,19 @@ def update_graph(selected_country, selected_contrincante=None):
         pie_title = f'Resultados de Batallas para {selected_country}'
         fig_subtitle = f'Se muestra las batallas en las que {selected_country} ha estado involucrado. El color azul representa el número de veces que {selected_country} se defendió de ataques de otros países, mientras que el color rojo indica las ocasiones en que atacó a esos países.'
         pie_subtitle = f''
-        return [fig, pie_fig, fig_title, pie_title,fig_subtitle,pie_subtitle]
+        return [fig, pie_fig, fig_title, pie_title, fig_subtitle, pie_subtitle]
     else:
         battles = pd.read_csv('data/battles_clean.csv')
-        df_conflicts = battles[['isqno', 'attacker', 'defender','winner']]
-        df_conflicts = df_conflicts.set_index(['isqno']) 
+        df_conflicts = battles[['isqno', 'attacker', 'defender', 'winner']]
+        df_conflicts = df_conflicts.set_index(['isqno'])
 
         country1 = selected_country
-        df_conflicts = df_conflicts[(df_conflicts['attacker'] == country1) | (df_conflicts['defender'] == country1)]
+        df_conflicts = df_conflicts[(df_conflicts['attacker'] == country1) | (
+            df_conflicts['defender'] == country1)]
 
         country2 = selected_contrincante
-        df_conflicts = df_conflicts[(df_conflicts['attacker'] == country2) | (df_conflicts['defender'] == country2)]
+        df_conflicts = df_conflicts[(df_conflicts['attacker'] == country2) | (
+            df_conflicts['defender'] == country2)]
 
         for index, row in df_conflicts.iterrows():
             if row['winner'] == 'attacker':
@@ -165,19 +168,22 @@ def update_graph(selected_country, selected_contrincante=None):
                 df_conflicts.at[index, 'country_winner'] = row['defender']
             else:
                 df_conflicts.at[index, 'country_winner'] = 'draw'
-            
-        
-        conflict_count = df_conflicts['attacker'].value_counts().to_frame().reset_index()
+
+        conflict_count = df_conflicts['attacker'].value_counts(
+        ).to_frame().reset_index()
         conflict_count.columns = ['country', 'attacker_count']
-        conflict_count = conflict_count.sort_values(by='attacker_count', ascending=False)
+        conflict_count = conflict_count.sort_values(
+            by='attacker_count', ascending=False)
         attacks_count = conflict_count[conflict_count['country'] == country1]
         defends_count = conflict_count[conflict_count['country'] == country2]
         try:
-            attack_data2 = {country2: attacks_count['attacker_count'].values[0]}
+            attack_data2 = {
+                country2: attacks_count['attacker_count'].values[0]}
         except:
             attack_data2 = {country2: 0}
         try:
-            defense_data2 = {country2: -defends_count['attacker_count'].values[0]}
+            defense_data2 = {country2: -
+                             defends_count['attacker_count'].values[0]}
         except:
             defense_data2 = {country2: 0}
 
@@ -218,25 +224,29 @@ def update_graph(selected_country, selected_contrincante=None):
             'plot_bgcolor': 'rgba(0, 0, 0, 0)',
             'paper_bgcolor': 'rgba(0, 0, 0, 0)',
         })
-        wins = df_conflicts.groupby(['country_winner']).size().reset_index(name='wins')
+        wins = df_conflicts.groupby(
+            ['country_winner']).size().reset_index(name='wins')
         wins = wins.rename(columns={'country_winner': 'country'})
         try:
-            win_data = {country1: wins[wins['country'] == country1]['wins'].values[0]}
+            win_data = {
+                country1: wins[wins['country'] == country1]['wins'].values[0]}
         except:
             win_data = {country1: 0}
         try:
-            lose_data = {country2: wins[wins['country'] == country2]['wins'].values[0]}
+            lose_data = {
+                country2: wins[wins['country'] == country2]['wins'].values[0]}
         except:
             lose_data = {country2: 0}
         if 'draw' in wins['country'].values:
-            draw_data = {'draw': wins[wins['country'] == 'draw']['wins'].values[0]}
+            draw_data = {'draw': wins[wins['country']
+                                      == 'draw']['wins'].values[0]}
         else:
             draw_data = {'draw': 0}
 
         pie_fig = go.Figure(data=[go.Pie(
             labels=[
-                f'Ganadas: {win_data[country1]}', 
-                f'Perdidas: {lose_data[country2]}', 
+                f'Ganadas: {win_data[country1]}',
+                f'Perdidas: {lose_data[country2]}',
                 f'Empatadas: {draw_data["draw"]}'
             ],
             values=[win_data[country1], lose_data[country2], draw_data['draw']],
@@ -260,4 +270,4 @@ def update_graph(selected_country, selected_contrincante=None):
         pie_title = f'Resultados de Batallas para {selected_country} contra {selected_contrincante}'
         fig_subtitle = f'Se muestra las batallas en las que {selected_country} ha estado involucrado contra {selected_contrincante}. El color azul representa el número de veces que {selected_country} se defendió de ataques de {selected_contrincante}, mientras que el color rojo indica las ocasiones en que lo atacó.'
         pie_subtitle = f''
-        return [fig,pie_fig,fig_title,pie_title,fig_subtitle,pie_subtitle]
+        return [fig, pie_fig, fig_title, pie_title, fig_subtitle, pie_subtitle]
