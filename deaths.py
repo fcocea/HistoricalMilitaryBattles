ae399a0ce889100f4f2e6d7d5a714277354916df
Luis Valenzuela <lvalenzuel2020@udec.cl>
2024-06-23 23:06:33 -0400

    modificacion de grafico de bigote y waffle

--- deaths.py
+++ deaths.py
@@ -6,38 +6,98 @@ import plotly.express as px
 from dash_manager import app
 from dash import Output, Input
 
-deaths = pd.read_csv('data/battles_clean.csv')
-deaths = deaths[['isqno', 'date_start', 'attacker', 'defender', 'deaths']]
-
+battles = pd.read_csv('data/battles_clean.csv')
+df_belligerents = pd.read_csv('data/belligerents.csv')
 
 @app.callback(
     [Output('boxplot_deaths', 'figure'), Output(
         'boxplot-deaths-title', 'children')],
-    [Input('country-dropdown', 'value')]
+    [Input('country-dropdown', 'value'),
+     Input('contrincante-dropdown', 'value')]
 )
-def boxplot_deaths(selected_country):
-    deaths_country = deaths.loc[(deaths['attacker'] == selected_country) | (
-        deaths['defender'] == selected_country)].copy()
-    deaths_country.loc[:, 'year'] = pd.to_datetime(
-        deaths_country['date_start']).dt.year
-    deaths_country = deaths_country[['year', 'deaths']]
-    deaths_country = deaths_country.groupby('year').sum().reset_index()
+def boxplot_deaths(selected_country, selected_contrincante=None):
+    if selected_contrincante is None:
 
-    deaths_country = deaths_country.set_index('year')
+        deaths = battles[(battles['attacker'] == selected_country) | (battles['defender'] == selected_country)]
+        deaths = deaths[['isqno', 'attacker', 'defender', 'date_start']]
+        deaths['date_start'] = pd.to_datetime(deaths['date_start'])
+        deaths = deaths.merge(df_belligerents, on='isqno')
+        deaths['country'] = np.where(deaths['attacker_y'] == 1, deaths['attacker_x'], deaths['defender'])
+        deaths.drop(columns=['attacker_x', 'attacker_y', 'defender','actors'], inplace=True)
+        deaths = deaths[deaths['country'] == selected_country]
+        deaths = deaths.dropna(subset=['cas'])
+        deaths = deaths[['isqno', 'cas', 'country']]
+        deaths = deaths.rename(columns={'cas': selected_country})
+        deaths = deaths.drop(columns=['country'])
+        deaths = deaths.set_index('isqno')
 
-    deaths_country.columns = [selected_country]
-    fig = px.box(deaths_country, x=selected_country,
-                 orientation='h', labels={selected_country: 'Muertes'})
-    fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+        fig = px.box(deaths, orientation='h')
+        # color #3399FF
+        fig.update_traces(marker_color='#3399FF')
+        fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
 
-    fig.update_xaxes(showgrid=False, showline=False)
-    fig.update_yaxes(showgrid=False, showline=False)
+        fig.update_xaxes(showgrid=False, showline=False)
+        fig.update_yaxes(showgrid=False, showline=False)
 
-    fig.update_yaxes(title_text=selected_country)
-    fig.update_traces(hoverinfo='none')
-    fig.update_layout({
-        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-    })
-    fig.update_layout(hovermode=False)
-    return [fig, f"Distribución de las muertes en batalla que involucran a {selected_country}"]
+        fig.update_yaxes(title_text='País')
+        fig.update_xaxes(title_text='Muertes en batalla')
+        fig.update_layout({
+            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        })
+        return [fig, f"Distribución de las muertes en batallas que involucran a {selected_country}"]
+    else:
+        # Filtra para obtener batallas donde ambos países están involucrados
+        deaths = battles[((battles['attacker'] == selected_country) & (battles['defender'] == selected_contrincante)) |
+                        ((battles['attacker'] == selected_contrincante) & (battles['defender'] == selected_country))]
+        deaths = deaths[['isqno', 'attacker', 'defender', 'date_start']]
+        deaths['date_start'] = pd.to_datetime(deaths['date_start'])
+        deaths = deaths.merge(df_belligerents, on='isqno')
+
+        deaths['country'] = np.where(deaths['attacker_y'] == 1, deaths['attacker_x'], deaths['defender'])
+        deaths.drop(['attacker_x', 'defender', 'attacker_y'], axis=1, inplace=True)
+        deaths = deaths[['isqno', 'country', 'date_start', 'cas']]
+
+        deaths_attacker = deaths[deaths['country'] == selected_country].copy()
+        deaths_attacker.rename(columns={'cas': selected_country}, inplace=True)
+        deaths_attacker.drop('country', axis=1, inplace=True)
+        # borrar date_start
+        deaths_attacker.drop('date_start', axis=1, inplace=True)
+        deaths_defender = deaths[deaths['country'] == selected_contrincante].copy()
+        deaths_defender.rename(columns={'cas': selected_contrincante}, inplace=True)
+        deaths_defender.drop('country', axis=1, inplace=True)
+        deaths_defender.drop('date_start', axis=1, inplace=True)
+
+        deaths_attacker = deaths_attacker.merge(deaths_defender, on='isqno')
+        deaths_attacker.set_index('isqno', inplace=True)
+        deaths_attacker_melted = deaths_attacker.reset_index().melt(id_vars=['isqno'], var_name='country', value_name='casualties')
+
+        fig = px.box(
+            deaths_attacker_melted, 
+            x='casualties', 
+            color='country',  # Indica la columna que define los colores
+            orientation='h',  # Orientación horizontal
+            color_discrete_map={
+                selected_country: '#3399FF',  # Azul para Independentistas
+                selected_contrincante: '#DC143C'              # Rojo para España
+            },
+            category_orders={"country": [selected_country, selected_contrincante]}
+        )
+        fig.update_layout(
+            showlegend=False,  # Ocultar la leyenda
+            plot_bgcolor='white', # Fondo blanco para el gráfico
+            yaxis=dict(
+                title='',  # Remover el título del eje Y
+                showgrid=True,  # Mostrar la rejilla
+                tickmode='array',
+                tickvals=[-0.17, 0.17],  # Posiciones de las etiquetas
+                ticktext=[selected_country,selected_contrincante]  # Texto de las etiquetas
+            )
+        )
+        fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+
+        fig.update_layout(yaxis_title="Países", xaxis_title="Muertes en batalla")
+        
+        return [fig, f"Distribución de las muertes en batallas de {selected_country} contra {selected_contrincante}"]
+
+
--- troops.py
+++ troops.py
@@ -4,141 +4,218 @@ from dash_manager import app
 from dash import Output, Input
 import plotly.graph_objects as go
 
-troops = pd.read_csv('data/battles_clean.csv')
-# troops = troops[['isqno', 'date_start','attacker', 'defender','soldiers','tanks','artillery','aircraft','cavalry']]
-troops = troops[['isqno', 'date_start', 'attacker',
-                 'defender', 'tanks', 'artillery', 'cavalry', 'aircraft']]
-countries = troops['attacker'].unique()
-countries = np.append(countries, troops['defender'].unique())
-countries = np.unique(countries)
+def create_waffle_chart(data, height, width, categories, colorscale, selected_country, y_offset):
+    total_values = sum(data['Total'])
+    category_proportions = {category: float(value) / total_values for category, value in zip(data.index, data['Total'])}
+    total_num_tiles = width * height
+    category_tiles = {category: round(proportion * total_num_tiles) for category, proportion in category_proportions.items()}
+    category_tiles = dict(sorted(category_tiles.items(), key=lambda item: item[1], reverse=True))
+
+    max_category = max(category_tiles, key=category_tiles.get)
+    if sum(category_tiles.values()) < total_num_tiles:
+        category_tiles[max_category] += total_num_tiles - sum(category_tiles.values())
+    elif sum(category_tiles.values()) > total_num_tiles:
+        category_tiles[max_category] -= sum(category_tiles.values()) - total_num_tiles
+
+    waffle_chart = np.zeros((height, width))
+    tile_index = 0
+    category_index = 0
+
+    z_values = {category: i for i, category in enumerate(categories)}
+    for col in range(width):
+        for row in range(height):
+            tile_index += 1
+            if tile_index > sum(list(category_tiles.values())[0:category_index]):
+                category_index += 1
+            waffle_chart[row, col] = z_values[list(category_tiles)[category_index-1]]
+    
+    return go.Heatmap(
+        z=waffle_chart,
+        x0=0, dx=1, y0=y_offset, dy=1,  # Comienza en y0=y_offset
+        colorscale=colorscale,
+        xgap=2,
+        ygap=2,
+        showscale=False,
+        hoverinfo='skip',
+        name=selected_country,
+        zmin=0,
+        zmax=3
+    )
+
+battles = pd.read_csv('data/battles_clean.csv')
+df_belligerents = pd.read_csv('data/belligerents.csv')
 
 
 @app.callback(
     [Output('battle-troops', 'figure'),
      Output('battle-troops-title', 'children')],
-    [Input('country-dropdown', 'value')]
+    [Input('country-dropdown', 'value'),
+     Input('contrincante-dropdown', 'value')]
 )
-def update_battle_troops(selected_country):
-    # Supongamos que tienes un DataFrame llamado 'troops'
-    filtered_troops = troops[(troops['attacker'] == selected_country) | (
-        troops['defender'] == selected_country)].copy()
-    filtered_troops.drop(
-        columns=['attacker', 'defender', 'date_start', 'isqno'], inplace=True)
-    filtered_troops = filtered_troops.transpose()
-    filtered_troops['Total'] = filtered_troops.sum(axis=1)
-    filtered_troops = filtered_troops[['Total']]
+def update_battle_troops(selected_country, selected_contrincante=None):
+    if selected_contrincante is None:
+        troops = battles[((battles['attacker'] == selected_country) | (battles['defender'] == selected_country))]
+        troops = troops[['isqno', 'attacker', 'defender', 'date_start']]
+        troops['date_start'] = pd.to_datetime(troops['date_start'])
+        troops = troops.merge(df_belligerents, on='isqno')
+        troops['country'] = np.where(troops['attacker_y'] == 1, troops['attacker_x'], troops['defender'])
+        troops.drop(['attacker_x', 'defender', 'attacker_y'], axis=1, inplace=True)
 
-    total_values = sum(filtered_troops['Total'])
-    category_proportions = {category: float(
-        value) / total_values for category, value in zip(filtered_troops.index, filtered_troops['Total'])}
-    width = 30
-    height = 20
-    total_num_tiles = width * height
+        troops = troops[troops['country'] == selected_country]
+        troops = troops[['country','tank', 'arty', 'fly', 'cav']]
+        troops.columns = ['country', 'tanks', 'artillery', 'aircraft', 'cavalry']
 
-    category_tiles = {category: round(proportion * total_num_tiles)
-                      for category, proportion in category_proportions.items()}
-    category_tiles = dict(sorted(category_tiles.items(),
-                          key=lambda item: item[1], reverse=True))
+        troops = troops.groupby(['country']).sum().reset_index()
+        troops = troops.drop('country', axis=1)
+        troops = troops.transpose()
 
-    max_category = max(category_tiles, key=category_tiles.get)
-    if sum(category_tiles.values()) < total_num_tiles:
-        category_tiles[max_category] += total_num_tiles - \
-            sum(category_tiles.values())
-    elif sum(category_tiles.values()) > total_num_tiles:
-        category_tiles[max_category] -= sum(
-            category_tiles.values()) - total_num_tiles
+        troops.columns = ['Total']
 
-    # inicializa el waffle chart como una matriz vacía
-    waffle_chart = np.zeros((height, width))
+        categories = ['aircraft', 'artillery', 'cavalry', 'tanks']
+        color_map = {
+            'aircraft': '#66C2A5',
+            'artillery': '#FC8D62',
+            'cavalry': '#8DA0CB',
+            'tanks': '#E78AC3'
+        }
+        colorscale = [
+            [0.00, color_map['aircraft']],  # color para el valor 1
+            [0.249, color_map['aircraft']],
+            [0.25, color_map['artillery']],   # color para el valor 2
+            [0.499, color_map['artillery']],
+            [0.50, color_map['cavalry']],  # color para el valor 3
+            [0.749, color_map['cavalry']],
+            [0.75, color_map['tanks']],  # color para el valor 4
+            [1.00, color_map['tanks']]
+        ]
 
-    categories = ['aircraft', 'artillery', 'cavalry', 'tanks']
-    z_values = {
-        'aircraft': 0,
-        'artillery': 1,
-        'cavalry': 2,
-        'tanks': 3
-    }
-    # Definición de la escala de colores
-    color_map = {
-        'aircraft': '#66C2A5',
-        'artillery': '#FC8D62',
-        'cavalry': '#8DA0CB',
-        'tanks': '#E78AC3'
-    }
-
-    colorscale = [
-        [0.00, color_map['aircraft']],  # color para el valor 1
-        [0.25, color_map['aircraft']],
-        [0.25, color_map['artillery']],   # color para el valor 2
-        [0.50, color_map['artillery']],
-        [0.50, color_map['cavalry']],  # color para el valor 3
-        [0.75, color_map['cavalry']],
-        [0.75, color_map['tanks']],  # color para el valor 4
-        [1.00, color_map['tanks']]
-    ]
-
-    translate_troop = {
-        'aircraft': 'Aéreo',
-        'artillery': 'Artilleria',
-        'cavalry': 'Caballería',
-        'tanks': 'Tanques'
-    }
-
-    tile_index = 0
-    category_index = 0
-
-    for col in range(width):
-        for row in range(height):
-            tile_index += 1
-
-            if tile_index > sum(list(category_tiles.values())[0:category_index]):
-                category_index += 1
-
-            waffle_chart[row, col] = z_values[list(
-                category_tiles)[category_index-1]]
-
-    fig = go.Figure()
-    fig.add_trace(go.Heatmap(
-        z=waffle_chart,
-        zmin=0,
-        zmax=3,
-        colorscale=colorscale,
-        xgap=2,
-        ygap=2,
-        showscale=False,
-        hoverinfo='skip'
-    ))
-
-    # Añadir trazos de Scatter para la leyenda utilizando colores específicos de category_color_map
-    for category in categories:
-        fig.add_trace(go.Scatter(
-            x=[None],
-            y=[None],
-            mode='markers',
-            # Usa el color específico para cada categoría
-            marker=dict(size=12, color=color_map[category]),
-            legendgroup=category,
-            showlegend=True,
-            name=translate_troop[category]
-        ))
-    fig.update_layout(
-        legend=dict(
-            orientation='h',
-            yanchor='bottom',
-            y=1.02,
-            xanchor='right',
-            x=0.7,
-            font=dict(size=14)
+        translate_troop = {
+            'aircraft': 'Aéreo',
+            'artillery': 'Artilleria',
+            'cavalry': 'Caballería',
+            'tanks': 'Tanques'
+        }
+        fig = go.Figure()
+        fig_country = create_waffle_chart(troops, 20, 30, categories, colorscale, selected_country, y_offset=0)
+        fig.add_trace(fig_country)
+        for category in categories:
+            fig.add_trace(go.Scatter(
+                x=[None],
+                y=[None],
+                mode='markers',
+                marker=dict(size=12, color=color_map[category]),
+                legendgroup=category,
+                showlegend=True,
+                name=translate_troop[category]
+            ))
+        fig.update_layout(
+            legend=dict(
+                orientation='h',
+                yanchor='bottom',
+                y=1.02,
+                xanchor='center',
+                x=0.5
+            ),
+            margin=dict(l=20, r=20, t=20, b=20),
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            plot_bgcolor='rgba(0,0,0,0)',
         )
-    )
 
-    fig.update_layout(
-        # title=f'Waffle Chart for {selected_country}',
-        margin=dict(l=20, r=20, t=20, b=20),
-        showlegend=True,
-        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        plot_bgcolor='rgba(0,0,0,0)',
-    )
+        return [fig, f'Proporción de tropas en batallas que involucran a {selected_country}']
+    else:
+        troops = battles[((battles['attacker'] == selected_country) & (battles['defender'] == selected_contrincante)) |
+                        ((battles['attacker'] == selected_contrincante) & (battles['defender'] == selected_country))]
+        troops = troops[['isqno', 'attacker', 'defender', 'date_start']]
+        troops['date_start'] = pd.to_datetime(troops['date_start'])
+        troops = troops.merge(df_belligerents, on='isqno')
 
-    return [fig, f'Waffle Chart {selected_country}']
+        troops['country'] = np.where(troops['attacker_y'] == 1, troops['attacker_x'], troops['defender'])
+        troops.drop(['attacker_x', 'defender', 'attacker_y'], axis=1, inplace=True)
+
+        troops = troops[['country','tank', 'arty', 'fly', 'cav']]
+        # rename columns tank a tanks, arty a artillery, fly a aircraft, cav a cavalry
+        troops.columns = ['country', 'tanks', 'artillery', 'aircraft', 'cavalry']
+
+        troops = troops.groupby(['country']).sum().reset_index()
+        # añadir columna total
+        #troops['total'] = troops['tank'] + troops['arty'] + troops['fly'] + troops['cav']
+        troops_attacker = troops[troops['country'] == selected_country]
+        troops_defender = troops[troops['country'] == selected_contrincante]
+        # quitamos la columna country
+        troops_attacker = troops_attacker.drop('country', axis=1)
+        troops_defender = troops_defender.drop('country', axis=1)
+
+        troops_attacker = troops_attacker.transpose()
+        troops_defender = troops_defender.transpose()
+
+        troops_attacker.columns = ['Total']
+        troops_defender.columns = ['Total']
+        categories = ['aircraft', 'artillery', 'cavalry', 'tanks']
+        color_map = {
+            'aircraft': '#66C2A5',
+            'artillery': '#FC8D62',
+            'cavalry': '#8DA0CB',
+            'tanks': '#E78AC3'
+        }
+        colorscale = [
+            [0.00, color_map['aircraft']],  # color para el valor 1
+            [0.249, color_map['aircraft']],
+            [0.25, color_map['artillery']],   # color para el valor 2
+            [0.499, color_map['artillery']],
+            [0.50, color_map['cavalry']],  # color para el valor 3
+            [0.749, color_map['cavalry']],
+            [0.75, color_map['tanks']],  # color para el valor 4
+            [1.00, color_map['tanks']]
+        ]
+
+        translate_troop = {
+            'aircraft': 'Aéreo',
+            'artillery': 'Artilleria',
+            'cavalry': 'Caballería',
+            'tanks': 'Tanques'
+        }
+
+        fig = go.Figure()
+
+        # Añadir el waffle chart del atacante con un desplazamiento de y=0
+        fig_attacker = create_waffle_chart(troops_attacker, 10, 40, categories, colorscale, selected_country, y_offset=0)
+        fig.add_trace(fig_attacker)
+
+        # Añadir el waffle chart del defensor con un desplazamiento de y=-11 (ajuste según la necesidad)
+        fig_defender = create_waffle_chart(troops_defender, 10, 40, categories, colorscale, selected_contrincante, y_offset=-11)
+        fig.add_trace(fig_defender)
+        fig.add_annotation(
+            x=0.5, y=0.5, xref='paper', yref='paper',
+            showarrow=False, text=f"{selected_country}", font=dict(size=14, color='black'), bgcolor='rgba(255,255,255,0.8)')
+
+        fig.add_annotation(
+            x=0.5, y=1.03, xref='paper', yref='paper',
+            showarrow=False, text=f"{selected_contrincante}", font=dict(size=14, color='black'), bgcolor='rgba(255,255,255,0.8)')
+
+        for category in categories:
+            fig.add_trace(go.Scatter(
+                x=[None],
+                y=[None],
+                mode='markers',
+                marker=dict(size=12, color=color_map[category]),
+                legendgroup=category,
+                showlegend=True,
+                name=translate_troop[category]
+            ))
+
+        fig.update_layout(
+            legend=dict(
+                orientation='h',
+                yanchor='bottom',
+                y=1.02,
+                xanchor='center',
+                x=0.5
+            ),
+            margin=dict(l=20, r=20, t=20, b=20),
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            plot_bgcolor='rgba(0,0,0,0)',
+        )
+
+        return [fig, f'Proporción de tropas en las batallas de {selected_country} contra {selected_contrincante}']
